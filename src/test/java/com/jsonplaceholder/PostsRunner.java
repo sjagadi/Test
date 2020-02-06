3a72e21a64249c6ca37d18222aeb81edfacda3d4
Santoshkumar Jagadi <sjagadi@outlook.com>
2020-02-06 20:34:09 +0100

    second commit

--- src/test/java/com/jsonplaceholder/PostsRunner.java
+++ src/test/java/com/jsonplaceholder/PostsRunner.java
@@ -8,8 +8,7 @@ import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = "src/test/resources/features",
-					glue = "net.serenity_bdd.samples")
+@CucumberOptions(features = "src/test/resources/features")
 public class PostsRunner extends BaseTest
 {
 
